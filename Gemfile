7543ec7a9e3a840a4dba9677a59a1aca15d30dd7
Eric Lima <ericjlima@gmail.com>
2017-01-07 17:17:35 -0500

    done?

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'react-rails', '~> 1.0'
+
 
 
 group :development, :test do
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,13 +4,17 @@ class Post < ActiveRecord::Base
             presence: true
   validates :content,
             presence: true
+  validates :image,
+            presence: true
   validates :user,
             presence: true
   def to_s
     title
   end
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+                    length: {minimum: 5}
   #validates :title, presence: true,
   #                  length: { minimum: 5 }
   #end
